0bfe37c6ea72b5a0a57c707ee38a2bc4e8709da4
이상목 <yoochang4176@naver.com>
2024-06-09 19:00:56 +0900

    Feat: "Main Activity DatePicker 이용한 버튼 클릭 이벤트 구현"

--- app/src/main/java/com/sangmoki/d_day/MainActivity.kt
+++ app/src/main/java/com/sangmoki/d_day/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.sangmoki.d_day
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,55 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        
+        // 시작일 종료일 객체 생성
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        val endBtn = findViewById<Button>(R.id.endBtn)
+
+        val dayText = findViewById<TextView>(R.id.dayText)
+
+        // 시작일 종료일 객체 생성
+        var startDate = ""
+        var endDate = ""
+        
+        // 시작일 버튼 클릭 이벤트
+        startBtn.setOnClickListener {
+            
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dalog = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    startDate = "${year}년 ${month + 1}월 ${dayOfMonth}일"
+                    startDate = year.toString() + (month + 1).toString() + dayOfMonth.toString()
+                }
+            }, year, month, day)
+
+            dalog.show()
+        }
+        
+        // 종료일 버튼 클릭 이벤트
+        endBtn.setOnClickListener {
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dalog = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+//                    endDate = "${year}년 ${month + 1}월 ${dayOfMonth}일"
+                    endDate = year.toString() + (month + 1).toString() + dayOfMonth.toString()
+
+                    findViewById<TextView>(R.id.dayText).setText(endDate.toInt() - startDate.toInt())
+                }
+            }, year, month, day)
+
+            dalog.show()
         }
     }
 }
